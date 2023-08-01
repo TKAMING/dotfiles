428775cc3304365f71c8429db56564a7e0a03298
Tobias Karuth <98485613+TKAMING@users.noreply.github.com>
2023-08-01 18:32:47 +0200

    Update backup-dotfiles.sh

--- backup-dotfiles.sh
+++ backup-dotfiles.sh
@@ -3,7 +3,7 @@
 # created by : cosmo (modified by tkaming)
 
 cp -r /home/$USER/.config/i3 /home/$USER/workflow/dotfiles/
-cp -r /home/$USER/.config/nvim /home/$USER/workflow/dotfilesjk
+cp -r /home/$USER/.config/nvim /home/$USER/workflow/dotfiles
 cp -r /home/$USER/.bash_aliases /home/$USER/workflow/dotfiles
 cp -r /home/$USER/.bashrc /home/$USER/workflow/dotfiles
 
